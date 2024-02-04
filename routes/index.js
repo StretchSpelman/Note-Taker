18b473e6845590b8a7f53d039960310c5491834e
Stretch Spelman <codingspelman@gmail.com>
2024-02-04 16:29:28 -0700

    created get routes

--- routes/index.js
+++ routes/index.js
@@ -15,3 +15,21 @@ function readData() {
 function writeData(data) {
   fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), "utf8");
 }
+
+router.get("/notes", (req, res) => {
+    const notes = readData();
+    res.json(notes);
+  });
+  
+ 
+  router.get("/notes/:id", (req, res) => {
+    const noteId = parseInt(req.params.id);
+    const notes = readData();
+    const note = notes.find((n) => n.id === noteId);
+  
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(404).json({ error: "Note not found" });
+    }
+  });
